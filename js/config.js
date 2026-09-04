307456d1ec5606d41d9ebbec1d0ae832a175cdbc
natanael <nathaxx09@hotmail.com>
2026-09-03 22:08:53 -0300

    se agrega un congig de firebase para la demo

---
+++ js/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBUB4oQPl2WBCQXpnmPrOQCjI4ZewfWw74",
+  authDomain: "horario-gurises.firebaseapp.com",
+  databaseURL: "https://horario-gurises-default-rtdb.firebaseio.com",
+  projectId: "horario-gurises",
+  storageBucket: "horario-gurises.firebasestorage.app",
+  messagingSenderId: "966729222570",
+  appId: "1:966729222570:web:2d8ca909f0e301ab3003d3"
+};
